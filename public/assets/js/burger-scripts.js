abb68b87fab3edf50565a636293c415510c12004
joshua-unseen <corpselight@gmail.com>
2019-06-02 07:47:05 -0700

    fixed some goofy form handling

--- public/assets/js/burger-scripts.js
+++ public/assets/js/burger-scripts.js
@@ -2,20 +2,27 @@ $(function () {
     $(".eat-it").on("click", function (event) {
         var id = $(this).data("id");
         $.ajax("/api/burgers/" + id,
-        {type: "PUT"})
-        .then(function () {
-            console.log("Burger id " + id + " devoured.");
-            location.reload();
-        });
+            { type: "PUT" })
+            .then(function () {
+                console.log("Burger id " + id + " devoured.");
+                location.reload();
+            });
     });
 
     $("#add-burger").on("submit", function (event) {
+        event.preventDefault();
         var newBurger = $("input[name='burger-name']").val().trim();
-        console.log(newBurger);
-        $.post("/api/burgers", {burgerName: newBurger})
-        .then(function(res) {
-            console.log("Burger " + res.id + " added.")
-        });
-        // event.preventDefault();
+        if (newBurger.length) {
+            var reqBody = { burgerName: newBurger };
+            console.log(newBurger);
+            $.ajax("/api/burgers", {
+                type: "POST",
+                data: reqBody
+            })
+                .then(function (res) {
+                    console.log("Burger " + res.id + " added.")
+                });
+            location.reload();
+        }
     });
 });
